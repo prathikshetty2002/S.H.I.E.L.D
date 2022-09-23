c1bc924d1049e52225b30a091631f988e4a343fa
Roshan <rmp99036@gmail.com>
2022-09-23 16:04:05 +0530

    navbar update

--- www.shield.com/components/Navbar.tsx
+++ www.shield.com/components/Navbar.tsx
@@ -1,25 +1,145 @@
-import { Box, Container, Text } from "@chakra-ui/react"
+import { Box, Button, Container, Heading, Input, ModalBody, ModalFooter, ModalHeader, Text, useDisclosure, VStack } from "@chakra-ui/react"
+import { HamburgerIcon } from '@chakra-ui/icons'
+import { auth } from '../firebase'
 
-const Navbar:React.FC = () => {
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalCloseButton,
+    useToast
+} from '@chakra-ui/react'
+
+import {
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+} from '@chakra-ui/react'
+import React, { useState } from "react"
+
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+} from '@chakra-ui/react'
+import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth"
+
+const Navbar: React.FC = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const { isOpen: isOpenmodal, onOpen: onOpenmodal, onClose: onClosemodal } = useDisclosure()
+    const btnRef = React.useRef()
+    const toast = useToast()
+    const [number, setnumber] = useState()
+    const [loading, setLoading] = useState(false)
+
+    const handleClick = async () => {
+        if (number.length != 10) {
+            toast({
+                title: 'Invalid Number',
+                description: "Number length mismatch",
+                status: 'error',
+                duration: 6000,
+                isClosable: true,
+            })
+            return
+        }
+
+        window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {
+            'size': 'normal',
+            'callback': (response) => {
+               onSigninSubmit()
+            }
+        }, auth);
+    }
+
+       function onSigninSubmit(){
+        const phoneNumber = "+91" +  number;
+        const appVerifier = window.recaptchaVerifier;
+        signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+            .then((confirmationResult) => {
+                // SMS sent. Prompt user to type the code from the message, then sign the
+                // user in with confirmationResult.confirm(code).
+                window.confirmationResult = confirmationResult;
+                alert("otp sended")
+                // ...
+            }).catch((error) => {
+                // Error; SMS not sent
+                // ...
+            });
+    }
+
+    function verifyotp(){
+        
+    }
 
     return (
-        <Box w="full" bg={"aqua"} display="flex" justifyContent={"space-between"} >
-            <Box>
-                S.H.I.E.L.D
-            </Box>
-            <Box display={"flex"}>
+        <>
+            <Box p={3} alignItems="center" w="full" bg={"aqua"} display="flex" justifyContent={"space-between"} >
                 <Box>
-                    <Text>Community</Text>
+                    <Heading fontSize={"20px"}>S.H.I.E.L.D</Heading>
                 </Box>
-                <Box>
-                    <Text>Text2</Text>
-                </Box>
-                <Box>
-                    <Text>Text3</Text>
-                </Box>
-            </Box>
+                <Button bg="aqua" onClick={onOpen} ref={btnRef} display={"flex"}>
+                    <HamburgerIcon h={7} w={7} />
+                </Button>
 
-        </Box>
+            </Box>
+            <Drawer
+                isOpen={isOpen}
+                placement='right'
+                onClose={onClose}
+                finalFocusRef={btnRef}
+            >
+                <DrawerOverlay />
+                <DrawerContent>
+                    <DrawerCloseButton />
+                    <DrawerHeader>S.H.I.E.L.D</DrawerHeader>
+
+                    <DrawerBody>
+                        <VStack w="full" >
+                            <Box w="full" p={2}><Text fontSize={"18px"}>Home</Text></Box>
+                            <Box w="full" p={2}><Text fontSize={"18px"}>Profile</Text></Box>
+                            <Box w="full" p={2}><Text fontSize={"18px"}>Community</Text></Box>
+                            <Box w="full" p={2}><Text fontSize={"18px"}>Explore Safe spot</Text></Box>
+                            <Box w="full" p={2}><Text fontSize={"18px"}>Donation</Text></Box>
+                            <Box w="full" p={2}><Text fontSize={"18px"}>Reedem</Text></Box>
+
+                        </VStack>
+                    </DrawerBody>
+                    {/* home ,profile , community ,explore safe spot , donation, redeem */}
+                    <DrawerFooter>
+                        <Button onClick={onOpenmodal} bg="blue" color={"white"} w="full" fontSize={"18px"}>Login</Button>
+                    </DrawerFooter>
+                </DrawerContent>
+            </Drawer>
+
+            <Modal maxW="90%" isOpen={isOpenmodal} onClose={onClosemodal}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Modal Title</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <FormControl isRequired>
+                            <FormLabel>Phone number</FormLabel>
+                            <Input onChange={(e) => setnumber(e.target.value)} type='number' placeholder="Enter phone number" />
+                            <FormHelperText>We'll never share your email.</FormHelperText>
+                            <div id="recaptcha-container"></div>
+                        </FormControl>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme='blue' mr={3} onClick={onClosemodal}>
+                            Close
+                        </Button>
+                        <Button variant='ghost' isLoading={loading} onClick={handleClick}>Submit</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
     )
 }
 
