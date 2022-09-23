6510a8dac2a86bb8524d5c2560930c648aa77fc1
Roshan <rmp99036@gmail.com>
2022-09-23 22:24:24 +0530

    ugh

---
+++ www.shield.com/pages/reportIncident.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+import Navbar from '../components/Navbar'
+
+
+const reportIncident: NextPage = () => {
+  return (
+   <Navbar/>
+  )
+}
+
+export default reportIncident
