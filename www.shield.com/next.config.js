d60cc3a6dcc8d1a56cc1e3b925e3a1c54f7be55e
Roshan <rmp99036@gmail.com>
2022-09-23 13:10:01 +0530

    chakra setup done

--- www.shield.com/next.config.js
+++ www.shield.com/next.config.js
@@ -1,26 +1,7 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true,
-  // setup envs for emailjs and gtag
-  // env: {
-  //   EMAILJS_CONTACT_SERVICE_ID: '',
-  //   EMAILJS_CONTACT: '',
-  //   EMAILJS_USERID: '',
-  //   GA_TRACKING_ID: ""
-  // },
-  async headers() {
-    return [
-      {
-        source: '/:all*(svg|jpg|png|jpeg)',
-        locale: false,
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=9999999999, must-revalidate',
-          }
-        ],
-      },
-    ]
-  },
+  swcMinify: true,
 }
+
+module.exports = nextConfig
