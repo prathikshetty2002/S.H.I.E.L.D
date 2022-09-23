e098a38b4fbe6144d96e0b503742e9ad710d27b6
Roshan <rmp99036@gmail.com>
2022-09-23 20:04:03 +0530

    soln

--- www.shield.com/firebase.js
+++ www.shield.com/firebase.js
@@ -4,6 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 
+
 const firebaseConfig= {
   apiKey:process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain:process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
