a6e4b2f2ecb2d2c598d34e9efdaf607547db1c6d
Mr-Jayesh <jayeshpotlabattini@gmail.com>
2022-09-23 17:52:29 +0530

    firebase setup done

--- www.shield.com/firebase.js
+++ www.shield.com/firebase.js
@@ -1,19 +1,26 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import {getAuth} from 'firebase/auth'
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCgBmgG8xuQkIa9rvynRBN40MZiYJPFJ-g",
-  authDomain: "shield-4bb39.firebaseapp.com",
-  projectId: "shield-4bb39",
-  storageBucket: "shield-4bb39.appspot.com",
-  messagingSenderId: "871641303352",
-  appId: "1:871641303352:web:173d8e2f05c537f70619cd",
-  measurementId: "G-FJ9CL387R6"
-};
+
+if (typeof window !== 'undefined' && !getApps().length) {
+
+initializeApp( {
+  apiKey:process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain:process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId:process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket:process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId:process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId:process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+});
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+const app = getApp()
 const auth = getAuth(app)
-export {auth}
+const firestore = getFirestore(app)
+const storage = getStorage(app)
+export {auth, firestore, storage};
