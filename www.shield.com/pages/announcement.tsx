5ddef71bae0ea003de53b8afbac62b321ca136da
Prathik Shetty <63772910+prathikshetty2002@users.noreply.github.com>
2023-03-23 12:58:14 +0530

    Update announcement.tsx

--- www.shield.com/pages/announcement.tsx
+++ www.shield.com/pages/announcement.tsx
@@ -15,13 +15,13 @@ const announcement: NextPage = () => {
                     <Heading>Important announcements</Heading>
                 </Box>
                 <Box w="90%" borderRadius={"14px"} p={3} boxShadow={"0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}>
-                    <Text>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum facere error ullam temporibus, omnis voluptatem vel qui, earum amet maxime laboriosam quae inventore placeat beatae quod architecto explicabo unde assumenda?</Text>
+                    <Text>Attention residents of Dhule, heavy flooding is occurring in the area. Please remain in your homes and avoid traveling on flooded roads. If you are in a low-lying area, please move to higher ground. Emergency services are on alert and will provide assistance as needed</Text>
                 </Box>
                 <Box borderRadius={"14px"} p={3} boxShadow={"0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"} w="90%" >
-                    <Text>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum facere error ullam temporibus, omnis voluptatem vel qui, earum amet maxime laboriosam quae inventore placeat beatae quod architecto explicabo unde assumenda?</Text>
+                    <Text>Attention residents, a wildfire is approaching the area. Please evacuate immediately and follow the designated evacuation routes. Take only essential items with you and leave pets and livestock behind, if necessary. Stay tuned to local news and social media for updates on the fire's progress.</Text>
                 </Box>
                 <Box borderRadius={"14px"} p={3} boxShadow={"0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"} w="90%">
-                    <Text>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum facere error ullam temporibus, omnis voluptatem vel qui, earum amet maxime laboriosam quae inventore placeat beatae quod architecto explicabo unde assumenda?</Text>
+                    <Text>This is an earthquake alert for the region. Please drop, cover, and hold on until the shaking stops. If you are outside, move away from buildings and other structures. If you are driving, pull over to a safe area and stay inside your vehicle until the shaking stops.</Text>
                 </Box>
                 
             </VStack>
